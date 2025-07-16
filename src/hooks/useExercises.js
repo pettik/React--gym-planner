8f62feb860d3ccbdb5a5c566d2a2ae3bfc026b33
pettik <petrbednarski@gmail.com>
2025-07-16 18:49:08 +0200

    Update useExercises.js

--- src/hooks/useExercises.js
+++ src/hooks/useExercises.js
@@ -1,8 +1,27 @@
 import { useState, useEffect} from 'react';
+import {exercises as seedExercises} from './../seedData';
 
 
 const useExcersices = () => {
-    return <div>A</div>
+
+    const addExercise = (exercise) => {
+
+    }
+    const updateExercise = (id, updatedFields) => {
+
+    }
+    const deleteExercise = (id) => {
+
+    }
+
+
+    const [exercises, setExercises] = useState(() => {
+        const stored = localStorage.getItem("exercises");
+        return stored ? JSON.parse(stored) : seedExercises;
+      });
+
+      return { exercises, addExercise, updateExercise, deleteExercise }
+
 
 }
 export default useExcersices;
